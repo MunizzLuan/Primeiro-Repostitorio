ada3395006083ef7a491b292476f701a2e45029e
unknown <luanmuniz88@gmail.com>
2022-02-16 15:33:10 -0300

    Comandos de Terminal

---
+++ Comandos/comandos.js
@@ -0,0 +1,9 @@
+Comandos terminal
+Dir = Listar todas as pastas do diretório 
+Mkdir = Criar pastas
+Cls = Limpar terminal
+Cd = Ir para diretório
+Cd..Voltar um diretório a esquerda
+Echo >> = Criar arquivo
+Rm = Apagar arquivo
+--version = Saber se algo ta instalado
