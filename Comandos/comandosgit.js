c8783459c6f55e472ce1dbb02c81b2ecc8ad05a0
unknown <luanmuniz88@gmail.com>
2022-02-16 15:45:40 -0300

    Comandos Git

---
+++ Comandos/comandosgit.js
@@ -0,0 +1,11 @@
+Comandos Git
+Git clone = clonar / baixar arquivo da nuvem, no computador local. 
+Git init = Inicializa um novo repositório local vazio.
+Git commit = checkpoint 
+Git push = salvar na nuvem
+Git pull = puxar nuvem salva   
+Git add. = Adicionar a área de preparação(Stage) pra mandar pra nuvem 
+Git reset = É o oposto do git add
+Git status = Ver o que tem na “Caixa”/Branch aqui conseguimos ver os arquivos na stage ou not stage
+Git commit = Cria um “marco / checkpoint” no projeto
+    (Branch ou ramificações é a duplicação do projeto sob o controle de versão que permite alterar partes do código separadamente)
